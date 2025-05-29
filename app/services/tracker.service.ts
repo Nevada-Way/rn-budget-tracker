e077cf42826a9e0611202ed8bd533337f503833a
riverdev <yariv.rivercybe@gmail.com>
2025-05-29 15:12:52 +0300

    wip: Adding the tower display of the budget state

---
+++ app/services/tracker.service.ts
@@ -0,0 +1,78 @@
+
+export type BudgetTrack = {
+	w:  number;
+	r:  number;
+	g:  number;
+	b2: number;
+	r2: number;
+};
+
+export function getBudgetTrack(): BudgetTrack {
+	try {
+		const res = {
+            w: 1,
+            r: 1,
+            g: 1,
+            b2: 1,
+            r2: 1,
+        }
+		return res;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+
+export function getBudgetTrackArray(): BudgetTrack[] {
+	try {
+		const res = [
+            {
+            w: 4,
+            r: 2,
+            g: 4,
+            b2: 2,
+            r2: 0,
+        },
+        {
+            w: 7,
+            r:2,
+            g: 1,
+            b2: 2,
+            r2: 0,
+        },
+        {
+            w: 8,
+            r: 2,
+            g: 0,
+            b2: 2,
+            r2: 0,
+        },
+        {
+            w: 10,
+            r: 0,
+            g: 0,
+            b2: 1.5,
+            r2: 0.5,
+        },
+        {
+            w: 10,
+            r: 0,
+            g: 0,
+            b2: 1,
+            r2: 1,
+        },
+        {
+            w: 10,
+            r: 0,
+            g: 0,
+            b2: 0,
+            r2: 2,
+        }
+    
+    ]
+		return res;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
